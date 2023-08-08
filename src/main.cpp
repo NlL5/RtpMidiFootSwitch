abe863c15c3ed711b71cef719192c65b3863be2a
Nils Mäser <nils@duzunglaublich.de>
2023-08-08 22:27:30 +0200

    Added LCD example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+#include "LiquidCrystal.h"
 
 #define SerialMon Serial
 #define APPLEMIDI_DEBUG SerialMon
@@ -24,6 +25,15 @@ const char* password = "g4OP9t@hbVeL";
   Example from WiFi > WiFiScan
   Complete details at https://RandomNerdTutorials.com/esp32-useful-wi-fi-functions-arduino/
 */
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int RS = D2, EN = D3, d4 = D5, d5 = D6, d6 = D7, d7 = D8;
+
+// if you use the NodeMCU 12E the suggested pins are
+// const int RS = 4, EN = 0, d4 = 12 , d5 = 13, d6 = 15, d7 = 3;
+
+LiquidCrystal lcd(RS, EN, d4, d5, d6, d7);
 
 /*void loop() {
   Serial.println("scan start");
@@ -74,6 +84,13 @@ void initWiFi() {
   Serial.println(WiFi.localIP());
 }
 
+void initLcd() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
 
 // -----------------------------------------------------------------------------
 //
@@ -89,6 +106,8 @@ void setup()
   DBG(F("Add device named Arduino with Host"), WiFi.localIP(), "Port", AppleMIDI.getPort(), "(Name", AppleMIDI.getName(), ")");
   DBG(F("Select and then press the Connect button"));
   DBG(F("Then open a MIDI listener and monitor incoming notes"));
+
+  initLcd();
 
   MIDI.begin();
 
@@ -117,6 +136,12 @@ void setup()
 // -----------------------------------------------------------------------------
 void loop()
 {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+
   // Listen to incoming notes
   MIDI.read();
 
